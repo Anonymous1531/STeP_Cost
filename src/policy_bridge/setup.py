19a6c554ddc72d7228ee8da35b7189cfd121135f
Anonymous <284960118+Anonymous1531@users.noreply.github.com>
2026-09-12 17:55:27 +0900

    Update setup.py

--- src/policy_bridge/setup.py
+++ src/policy_bridge/setup.py
@@ -1,26 +1,27 @@
 from setuptools import find_packages, setup
 
-package_name = 'policy_bridge'
+package_name = "policy_bridge"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Anonymous',
-    maintainer_email='anonymous@example.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="Anonymous",
+    maintainer_email="anonymous@example.com",
+    description=(
+        "STeP-Cost runtime policy bridge for detour-gated semantic-motion "
+        "TTL persistence in Navigation2."
+    ),
+    license="Proprietary",
     entry_points={
-        'console_scripts': [
-            'policy_bridge = policy_bridge.policybridge:main',
+        "console_scripts": [
+            "policy_bridge = policy_bridge.policybridge:main",
         ],
     },
 )
